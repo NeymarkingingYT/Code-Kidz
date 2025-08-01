69ae4563653ada426f9b2a8b26872796595b3e30
NeymarkingingYT <NeymarkingingYT@gmail.com>
2025-07-31 18:16:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-var workspace = Blockly.inject('blocklyDiv', {
-  toolbox: document.getElementById('toolbox'),
-  zoom: {
-    controls: true,
-    wheel: true,
-    startScale: 0.8
-  },
-  trashcan: true
+// Inject Blockly
+const workspace = Blockly.inject('blocklyDiv', {
+  toolbox: document.getElementById('toolbox')
 });
 
-workspace.addChangeListener(() => {
+// Define blocks
+Blockly.defineBlocksWithJsonArray([
+  {
+    "type": "say_message",
+    "message0": "say %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "TEXT",
+        "text": "Hello!"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 160
+  }
+]);
+
+// Generate JS code for block
+Blockly.JavaScript['say_message'] = function(block) {
+  const msg = block.getFieldValue('TEXT');
+  return `alert("${msg}");\n`;
+};
+
+// Run button
+function runCode() {
   const code = Blockly.JavaScript.workspaceToCode(workspace);
-  console.clear();
-  console.log(code);
-});
+  try {
+    eval(code);
+  } catch (e) {
+    alert("Error: " + e);
+  }
+}
